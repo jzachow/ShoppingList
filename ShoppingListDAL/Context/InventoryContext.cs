915a7299947e08b6c7d93bed6bc734e08018f8ee
Justin <justin.zachow@gmail.com>
2020-10-21 21:42:40 -0400

    Initialize repo

---
+++ ShoppingListDAL/Context/InventoryContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ShoppingListDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingListDAL
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(p =>
+            {
+                p.HasKey(_ => _.Id);
+
+                p.Property(_ => _.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+
+                p.Property(_ => _.Price)
+                .IsRequired();
+            });   
+        }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<InventoryContext>
+    {
+        public InventoryContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Database=Inventory;Trusted_Connection=true";
+            var builder = new DbContextOptionsBuilder<InventoryContext>();
+
+            builder.UseSqlServer(connectionString);
+
+            return new InventoryContext(builder.Options);
+        }
+    }
+}
---
+++ ShoppingListDAL/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ShoppingListDAL.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
